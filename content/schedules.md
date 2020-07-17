d86731ccdc2867327a7b8de5265a2a643ef99830
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-07-17 21:35:26 +0800

    Created content and page for displaying mass schedules.

---
+++ content/schedules.md
@@ -0,0 +1,25 @@
+---
+title: Getting started
+description: 'Empower your NuxtJS application with @nuxt/content module: write in a content/ directory and fetch your Markdown, JSON, YAML and CSV files through a MongoDB like API, acting as a Git-based Headless CMS.'
+---
+
+**Saturdays**
+
+Anticipated masses:
+
+* 5:00 PM
+* 6:15 PM
+
+**Sundays**
+
+Morning masses: 
+
+* 7:00 AM
+* 9:00 AM
+* 11:00 AM
+
+<BR />
+
+Afternoon masses:
+
+* 5:00 PM
---
+++ pages/schedules.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout>
+    <v-card style="width: 100%">
+      <v-card-title>Mass Schedules</v-card-title>
+      <v-card-text>
+        <nuxt-content :document="page"></nuxt-content>
+      </v-card-text>
+    </v-card>
+  </v-layout>
+</template>
+<script>
+  export default {
+      async asyncData({ $content }) {
+          const page = await $content('schedules').fetch()
+
+          return {
+              page
+          }
+      }
+  }
+</script>
