2a5e0d1653558a817371ed685125d8debbfa48d5
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-07-17 21:34:50 +0800

    Reworked layout of main page as the attendance sheet tracker.

--- pages/index.vue
+++ pages/index.vue
@@ -9,89 +9,63 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
+
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Attendance Sheet
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <p>This attendance sheet is intended for recording for <strong>July 19, 2020, Sunday</strong> for the time slot <strong>5:00 PM</strong>.</p>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
+        <v-row align="center">
+          <v-form ref="form" class="form-contact-tracing">
+            <v-text-field v-model="formData.name" class="form-field" required label="Mass Attendee's Name">
+            </v-text-field>
+            <v-text-field v-model="formData.contactNumber" class="form-field" required label="Contact Number">
+            </v-text-field>
+            <v-textarea v-model="formData.address" class="form-field" required label="Address">
+            </v-textarea>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn nuxt
+                  to="/records"
+              >
+                View records
+              </v-btn>
+              <v-btn
+                  color="primary"
+                  nuxt
+                  @click="submit()"
+              >
+                Submit
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-row>
       </v-card>
+
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
-  }
+  },
+    data() {
+      return {
+          formData: {
+              name: "",
+              address: "",
+              contactNumber: ""
+          }
+      }
+    },
+    methods: {
+      submit() {
+          console.log("Hello world");
+      }
+    }
 }
 </script>
